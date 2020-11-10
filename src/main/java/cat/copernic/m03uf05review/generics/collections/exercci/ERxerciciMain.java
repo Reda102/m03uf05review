a0bf8cdcf2f720aa410ad1e5a0b1f28c7469a2ee
Reda <reda.ieslaslagunas@gmail.com>
2020-11-10 18:14:06 +0100

    Ejercicio 3 Matcher  M03 UF5

--- src/main/java/cat/copernic/m03uf05review/generics/collections/exercci/ERxerciciMain.java
+++ src/main/java/cat/copernic/m03uf05review/generics/collections/exercci/ERxerciciMain.java
@@ -5,6 +5,9 @@
  */
 package cat.copernic.m03uf05review.generics.collections.exercci;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author pep
@@ -16,6 +19,20 @@ public class ERxerciciMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        MatcherImpl match = new MatcherImpl();
+
+        Persona prson = new Persona(10, Sexo.HOMBRE, 23, true, OrientacionSexual.HETERO, Formacion.GRADO_SUPERIOR, OrientacionPolitica.IZQUIERDA);
+        List<Persona> listMatcher = new ArrayList<>();
+        listMatcher.add(new Persona(01, Sexo.HOMBRE, 44, true, OrientacionSexual.HETERO, Formacion.GRADO, OrientacionPolitica.IZQUIERDA));
+        listMatcher.add(new Persona(02, Sexo.HOMBRE, 19, false, OrientacionSexual.HETERO, Formacion.DOCTORADO, OrientacionPolitica.IZQUIERDA));
+        listMatcher.add(new Persona(03, Sexo.HOMBRE, 59, false, OrientacionSexual.GAY, Formacion.GRADO_MEDIA, OrientacionPolitica.IZQUIERDA));
+        listMatcher.add(new Persona(04, Sexo.MUJER, 22, true, OrientacionSexual.HETERO, Formacion.GRADO, OrientacionPolitica.IZQUIERDA));
+        listMatcher.add(new Persona(05, Sexo.MUJER, 26, true, OrientacionSexual.HETERO, Formacion.GRADO_SUPERIOR, OrientacionPolitica.CENTRO));
+        listMatcher.add(new Persona(06, Sexo.MUJER, 65, false, OrientacionSexual.BISEXUAL, Formacion.CATEDRATICO, OrientacionPolitica.IZQUIERDA));
+
+        Persona Maach = match.matches(prson, listMatcher);
+
+        System.out.println(Maach);
     }
-    
+
 }
--- src/main/java/cat/copernic/m03uf05review/generics/collections/exercci/Persona.java
+++ src/main/java/cat/copernic/m03uf05review/generics/collections/exercci/Persona.java
@@ -5,12 +5,14 @@
  */
 package cat.copernic.m03uf05review.generics.collections.exercci;
 
+import java.util.Objects;
+
 /**
  *
  * @author pep
  */
-public class Persona {
-    
+public class Persona implements Comparable<Persona> {
+
     private int id;
     private Sexo sexo;
     private int edad;
@@ -18,6 +20,130 @@ public class Persona {
     private OrientacionSexual orientacionSexual;
     private Formacion formacion;
     private OrientacionPolitica orientacionPolitica;
-        
-    
+
+    public Persona(int id, Sexo sexo, int edad, boolean fumador, OrientacionSexual orientacionSexual, Formacion formacion, OrientacionPolitica orientacionPolitica) {
+        this.id = id;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.fumador = fumador;
+        this.orientacionSexual = orientacionSexual;
+        this.formacion = formacion;
+        this.orientacionPolitica = orientacionPolitica;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isFumador() {
+        return fumador;
+    }
+
+    public void setFumador(boolean fumador) {
+        this.fumador = fumador;
+    }
+
+    public OrientacionSexual getOrientacionSexual() {
+        return orientacionSexual;
+    }
+
+    public void setOrientacionSexual(OrientacionSexual orientacionSexual) {
+        this.orientacionSexual = orientacionSexual;
+    }
+
+    public Formacion getFormacion() {
+        return formacion;
+    }
+
+    public void setFormacion(Formacion formacion) {
+        this.formacion = formacion;
+    }
+
+    public OrientacionPolitica getOrientacionPolitica() {
+        return orientacionPolitica;
+    }
+
+    public void setOrientacionPolitica(OrientacionPolitica orientacionPolitica) {
+        this.orientacionPolitica = orientacionPolitica;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", sexo=" + sexo + ", edad=" + edad + ", fumador=" + fumador + ", orientacionSexual=" + orientacionSexual + ", formacion=" + formacion + ", orientacionPolitica=" + orientacionPolitica + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.id;
+        hash = 79 * hash + Objects.hashCode(this.sexo);
+        hash = 79 * hash + this.edad;
+        hash = 79 * hash + (this.fumador ? 1 : 0);
+        hash = 79 * hash + Objects.hashCode(this.orientacionSexual);
+        hash = 79 * hash + Objects.hashCode(this.formacion);
+        hash = 79 * hash + Objects.hashCode(this.orientacionPolitica);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.edad != other.edad) {
+            return false;
+        }
+        if (this.fumador != other.fumador) {
+            return false;
+        }
+        if (this.sexo != other.sexo) {
+            return false;
+        }
+        if (this.orientacionSexual != other.orientacionSexual) {
+            return false;
+        }
+        if (this.formacion != other.formacion) {
+            return false;
+        }
+        if (this.orientacionPolitica != other.orientacionPolitica) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int compareTo(Persona persona) { // comparamos mediante formacion politica,
+        return orientacionPolitica.compareTo(persona.orientacionPolitica);
+    }
+
 }
